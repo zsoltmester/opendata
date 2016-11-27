1678e1ad2e3706503da54e01799cc8cb1c957d20
zsmester <zsolt.mester67@gmail.com>
2016-11-27 17:26:23 +0100

    Improve validation

--- webapp/app/Http/Controllers/UserController.js
+++ webapp/app/Http/Controllers/UserController.js
@@ -16,9 +16,9 @@ class UserController {
 		const userData = request.only('username', 'password', 'email')
 
 		const rules = {
-			username: 'required',
+			username: 'required|unique:users',
 			password: 'required',
-			email: 'required'
+			email: 'required|email|unique:users'
 		}
 
 		const validation = yield Validator.validate(userData, rules)
@@ -27,7 +27,10 @@ class UserController {
 			yield request
 				.withOnly('username', 'password', 'email')
 				.andWith({
-					errors: validation.messages()
+					errors: validation.messages(),
+					username: userData.username,
+					password: userData.password,
+					email: userData.email
 				})
 				.flash()
 
@@ -42,8 +45,11 @@ class UserController {
 			yield request
 				.with({
 					errors: [{
-						message: "Failed to create user. Maybe the username or the email are not available."
-					}]
+						message: "Failed to create user. Maybe the username or the email are not available.",
+					}],
+					username: userData.username,
+					password: userData.password,
+					email: userData.email
 				})
 				.flash()
 
@@ -71,7 +77,9 @@ class UserController {
 			yield request
 				.withOnly('username', 'password')
 				.andWith({
-					errors: validation.messages()
+					errors: validation.messages(),
+					username: userData.username,
+					password: userData.password
 				})
 				.flash()
 
@@ -87,7 +95,9 @@ class UserController {
 				.with({
 					errors: [{
 						message: exception.message
-					}]
+					}],
+					username: userData.username,
+					password: userData.password
 				})
 				.flash()
 
@@ -108,44 +118,61 @@ class UserController {
 
 	*
 	modify(request, response) {
-		const userData = request.only('password', 'email')
+		const userData = request.only('username', 'password', 'email')
 
-		const rules = {
-			password: 'required',
-			email: 'required'
+		const passwordRules = {
+			password: 'required'
+		}
+		const passwordValidation = yield Validator.validate(userData, passwordRules)
+		if (!passwordValidation.fails()) {
+			request.currentUser.password = yield Hash.make(userData.password)
+			try {
+				yield request.currentUser.save()
+				yield request
+					.with({
+						infos: [{
+							message: "Your password modified successfully."
+						}]
+					})
+					.flash()
+			} catch (exception) {
+				yield request
+					.with({
+						errors: [{
+							message: "Failed to modify your password."
+						}]
+					})
+					.flash()
+			}
 		}
 
-		const validation = yield Validator.validate(userData, rules)
-
-		if (validation.fails()) {
-			yield request
-				.withOnly('password', 'email')
-				.andWith({
-					errors: validation.messages()
-				})
-				.flash()
-
-			response.redirect('back')
-			return
+		const emailRules = {
+			email: 'required|email|unique:users'
+		}
+		const emailValidation = yield Validator.validate(userData, emailRules)
+		if (!emailValidation.fails()) {
+			request.currentUser.email = userData.email
+			try {
+				yield request.currentUser.save()
+				yield request
+					.with({
+						infos: [{
+							message: "Your email modified successfully."
+						}]
+					})
+					.flash()
+			} catch (exception) {
+				yield request
+					.with({
+						errors: [{
+							message: "Failed to modify your email."
+						}]
+					})
+					.flash()
+			}
 		}
 
-		request.currentUser.password = yield Hash.make(userData.password)
-		request.currentUser.email = userData.email
-
-		try {
-			yield request.currentUser.save()
-			response.redirect('back')
-		} catch (exception) {
-			yield request
-				.with({
-					errors: [{
-						message: "Failed to modify your data. Maybe the email is not available."
-					}]
-				})
-				.flash()
-
-			response.redirect('back')
-		}
+		response.redirect('back')
 	}
 }
 
