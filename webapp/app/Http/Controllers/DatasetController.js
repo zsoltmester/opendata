8a029c9c0b2042433aef8ccf431e7ba3fb12b5c0
zsmester <zsolt.mester67@gmail.com>
2016-11-28 22:39:49 +0100

    Add create dataset process

--- webapp/app/Http/Controllers/DatasetController.js
+++ webapp/app/Http/Controllers/DatasetController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Dataset = use('App/Model/Dataset')
+const Validator = use('Validator')
 
 class DatasetController {
 
@@ -27,7 +28,52 @@ class DatasetController {
 
 	*
 	create(request, response) {
-		// TODO
+		const datasetData = request.only('summary', 'description', 'format', 'link', 'access')
+
+		const rules = {
+			summary: 'required|unique:datasets',
+			description: 'required',
+			format: 'required',
+			link: 'required',
+			access: 'required'
+		}
+
+		const validation = yield Validator.validate(datasetData, rules)
+
+		if (validation.fails()) {
+			yield request
+				.withAll()
+				.andWith({
+					errors: validation.messages()
+				})
+				.flash()
+
+			response.redirect('back')
+			return
+		}
+
+		try {
+			yield Dataset.create(datasetData)
+			yield request
+				.with({
+					infos: [{
+						message: "Dataset added successfully."
+					}]
+				})
+				.flash()
+			response.redirect('/')
+		} catch (exception) {
+			yield request
+				.withAll()
+				.andWith({
+					errors: [{
+						message: "Failed to add the dataset.",
+					}]
+				})
+				.flash()
+
+			response.redirect('back')
+		}
 	}
 }
 
